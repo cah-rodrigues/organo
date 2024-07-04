50eb5bbcc68e5a4a2ddb0fd778162a0d3c207beb
Carla Rodrigues <rodricah1.8@gmail.com>
2024-07-04 10:47:31 -0400

    Configuracoes iniciais do react, pasta de imagens, banner, campos de texto, formlario e botao de criar card

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+export const Banner = () => {
+    //JSX parte visual do react, como ele lê e transforma o elemento no DOM
+    return (
+        <header className = "banner">
+            <img src= "/imagens/banner.png" alt= "banner principal da página"/>
+        </header>
+    )
+}
+
---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,5 @@
+import { Banner } from "./Banner";
+
+export default Banner
+
+//vc pode criar componentes separados e aí exportar só os que quiser num unico index.js. São formas de organizar o código
